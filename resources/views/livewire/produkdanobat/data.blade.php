62ddfdcf22631a032d2235b2305fe7f0d5109f08
mnfds <daus2016.d2@gmail.com>
2025-07-17 16:17:25 +0800

    style: link dan icon pada breadcrumb

--- resources/views/livewire/produkdanobat/data.blade.php
+++ resources/views/livewire/produkdanobat/data.blade.php
@@ -1,26 +1,19 @@
 <div class="pt-1 pb-12">
     <div class="max-w-full mx-auto sm:px-6 lg:px-8 space-y-6">
         <!-- Breadcrumbs (hanya muncul di layar lg ke atas) -->
+        <!-- Breadcrumbs (hanya muncul di layar lg ke atas) -->
         <div class="hidden lg:flex justify-end px-4">
             <div class="breadcrumbs text-sm">
                 <ul>
                     <li>
-                        <a class="inline-flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 stroke-current" fill="none"
-                                viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                            </svg>
+                        <a href="{{ route('dashboard') }}" class="inline-flex items-center gap-1">
+                            <i class="fa-regular fa-folder"></i>
                             Dashboard
                         </a>
                     </li>
                     <li>
-                        <a class="inline-flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 stroke-current" fill="none"
-                                viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                            </svg>
+                        <a href="{{ route('produk-obat.data') }}" class="inline-flex items-center gap-1">
+                            <i class="fa-regular fa-folder-open"></i>
                             Produk Dan Obat
                         </a>
                     </li>
--- resources/views/livewire/users/data-users.blade.php
+++ resources/views/livewire/users/data-users.blade.php
@@ -6,22 +6,14 @@
                 <div class="breadcrumbs text-sm">
                     <ul>
                         <li>
-                            <a class="inline-flex items-center gap-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 stroke-current" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                                </svg>
+                            <a href="{{ route('dashboard') }}" class="inline-flex items-center gap-1">
+                                <i class="fa-regular fa-folder"></i>
                                 Dashboard
                             </a>
                         </li>
                         <li>
-                            <a class="inline-flex items-center gap-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 stroke-current" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                                </svg>
+                            <a href="{{ route('users.data') }}" class="inline-flex items-center gap-1">
+                                <i class="fa-regular fa-folder-open"></i>
                                 Staff
                             </a>
                         </li>
