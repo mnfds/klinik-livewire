25e5b627aaffeef4ec184353fd3fca2d7ee7ea61
mnfds <daus2016.d2@gmail.com>
2025-07-24 12:08:38 +0800

    feat: Store Dokter dengan Akun dan Poli terkait

--- app/Livewire/Dokter/Create.php
+++ app/Livewire/Dokter/Create.php
@@ -2,10 +2,142 @@
 
 namespace App\Livewire\Dokter;
 
+use App\Models\User;
+use App\Models\Dokter;
+use App\Models\DokterPoli;
+use App\Models\PoliKlinik;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class Create extends Component
 {
+    use WithFileUploads;
+
+    public $foto_wajah;
+    public $foto_wajah_preview;
+    public $ttd_digital;
+    public $ttd_digital_preview;
+
+    // Poli
+    public $id_poli;
+    public $poli = [];
+
+    // Akun
+    public $name;
+    public $email;
+    public $password;
+
+    // Biodata Dokter
+    public $nama_dokter;
+    public $alamat_dokter;
+    public $telepon;
+    public $jenis_kelamin;
+
+    public $tingkat_pendidikan;
+    public $institusi;
+    public $tahun_kelulusan;
+
+    public $no_str;
+    public $surat_izin_pratik;
+    public $masa_berlaku_sip;
+
+    public function mount()
+    {
+        $this->poli = PoliKlinik::orderBy('nama_poli')->pluck('nama_poli', 'id')->toArray();
+    }
+
+    public function store()
+    {
+        $data = $this->validate([
+            // Akun
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+
+            // Dokter
+            'nama_dokter' => 'required|string|max:255',
+            'alamat_dokter' => 'nullable|string|max:255',
+            'telepon' => 'nullable|string|max:20',
+            'jenis_kelamin' => 'required|in:L,P',
+
+            'tingkat_pendidikan' => 'nullable',
+            'institusi' => 'nullable',
+            'tahun_kelulusan' => 'nullable',
+
+            'no_str' => 'nullable',
+            'surat_izin_pratik' => 'nullable',
+            'masa_berlaku_sip' => 'nullable|date',
+
+            'foto_wajah' => 'nullable|image|max:1024',
+            'ttd_digital' => 'nullable|file|max:1024',
+        ]);
+
+        // dd($data);
+
+        $fotoPath = $this->foto_wajah
+            ? $this->foto_wajah->store('foto_wajah', 'public')
+            : null;
+
+        $ttdPath = $this->ttd_digital
+            ? $this->ttd_digital->store('ttd_digital_dokter', 'public')
+            : null;
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+            'role_id' => 2, // Role dokter
+        ]);
+
+        event(new Registered($user));
+
+
+
+        $dokter = Dokter::create([
+            'user_id' => $user->id,
+            'nama_dokter' => $this->nama_dokter,
+            'alamat_dokter' => $this->alamat_dokter,
+            'telepon' => $this->telepon,
+            'jenis_kelamin' => $this->jenis_kelamin,
+            'tingkat_pendidikan' => $this->tingkat_pendidikan,
+            'institusi' => $this->institusi,
+            'tahun_kelulusan' => $this->tahun_kelulusan,
+            'no_str' => $this->no_str,
+            'surat_izin_pratik' => $this->surat_izin_pratik,
+            'masa_berlaku_sip' => $this->masa_berlaku_sip,
+            'foto_wajah' => $fotoPath,
+            'ttd_digital' => $ttdPath,
+        ]);
+
+        DokterPoli::create([
+            'dokter_id' => $dokter->id,
+            'poli_id' => $this->id_poli,
+        ]);
+
+        session()->flash('toast', [
+            'type' => 'success',
+            'message' => 'Data Dokter berhasil disimpan.',
+        ]);
+
+        $this->resetForm();
+
+        return redirect()->route('dokter.data');
+    }
+
+    public function resetForm()
+    {
+        $this->reset([
+            'name', 'email', 'password', 'id_poli',
+            'nama_dokter', 'telepon', 'alamat_dokter','jenis_kelamin',
+            'tingkat_pendidikan', 'institusi', 'tahun_kelulusan',
+            'no_str', 'surat_izin_pratik', 'masa_berlaku_sip',
+            'foto_wajah', 'foto_wajah_preview',
+            'ttd_digital', 'ttd_digital_preview',
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.dokter.create');
--- resources/views/livewire/dokter/create.blade.php
+++ resources/views/livewire/dokter/create.blade.php
@@ -1,3 +1,212 @@
-<div>
-    p
+<div class="pt-1 pb-12">
+    <div class="max-w-full mx-auto sm:px-6 lg:px-8 space-y-6">
+        <!-- Breadcrumbs (hanya muncul di layar lg ke atas) -->
+        <div class="hidden lg:flex justify-end px-4">
+            <div class="breadcrumbs text-sm">
+                <ul>
+                    <li>
+                        <a wire:navigate href="{{ route('dashboard') }}" class="inline-flex items-center gap-1">
+                            <i class="fa-regular fa-folder"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a wire:navigate href="{{ route('dokter.data') }}" class="inline-flex items-center gap-1">
+                            <i class="fa-regular fa-folder"></i>
+                            Dokter
+                        </a>
+                    </li>
+                    <li>
+                        <a wire:navigate href="{{ route('dokter.create') }}" class="inline-flex items-center gap-1">
+                            <i class="fa-regular fa-folder-open"></i>
+                            Tambah Dokter
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page Title -->
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold text-base-content">
+                <i class="fa-solid fa-layer-group"></i>
+                Tambah Dokter
+            </h1>
+        </div>
+
+        <!-- Main Content -->
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+            <div class="bg-base-100 shadow rounded-box">
+                <form wire:submit.prevent="store" class="p-6 space-y-6">
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                        {{-- Username --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Username</span></label>
+                            <input wire:model.defer="name" id="name" name="name" type="text"
+                                required autofocus placeholder="username" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('name')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Email --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Email</span></label>
+                            <input wire:model.defer="email" type="email" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('email')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Password --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Password</span></label>
+                            <input wire:model.defer="password" type="password" class="input input-bordered w-full"
+                                placeholder="Isi password pengguna" />
+                            <x-input-error :messages="$errors->get('password')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Role --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Poli</span></label>
+                            <select wire:model.defer="id_poli" class="select select-bordered w-full">
+                                <option value="">Pilih Role</option>
+                                @foreach ($poli as $id => $name)
+                                    <option value="{{ $id }}">{{ $name }}</option>
+                                @endforeach
+                            </select>
+                            <x-input-error :messages="$errors->get('id_poli')" class="text-error text-sm mt-1" />
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                        {{-- Nama Lengkap --}}
+                        <div class="form-control">
+                            <label for="nama_dokter" class="label"><span class="label-text">Nama Lengkap</span></label>
+                            <input wire:model="nama_dokter" type="text" id="nama_dokter" name="nama_dokter"
+                                class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('nama_dokter')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Alamat --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Alamat</span></label>
+                            <input wire:model.defer="alamat_dokter" type="text" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('alamat_dokter')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Jenis Kelamin --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Jenis Kelamin</span></label>
+                            <select wire:model.defer="jenis_kelamin" class="select select-bordered w-full">
+                                <option value="">Pilih Jenis Kelamin</option>
+                                <option value="L">Laki-laki</option>
+                                <option value="P">Perempuan</option>
+                            </select>
+                            <x-input-error :messages="$errors->get('jenis_kelamin')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Telepon --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Telepon</span></label>
+                            <input wire:model.defer="telepon" type="text" inputmode="numeric" pattern="[0-9]*" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('telepon')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Institusi --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Institusi</span></label>
+                            <input wire:model.defer="institusi" type="text" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('institusi')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Tahun Kelulusan --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Tahun Kelulusan</span></label>
+                            <input wire:model.defer="tahun_kelulusan" type="date" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('tahun_kelulusan')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Tingkat Pendidikan --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Tingkat Pendidikan</span></label>
+                            <select wire:model.defer="tingkat_pendidikan" class="select select-bordered w-full">
+                                <option value="S1">S1</option>
+                                <option value="S2">S2</option>
+                                <option value="S3">S3</option>
+                            </select>
+                            <x-input-error :messages="$errors->get('tingkat_pendidikan')" class="text-error text-sm mt-1" />
+                        </div>
+                    
+                        {{-- No STR --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Nomor STR</span></label>
+                            <input wire:model.defer="no_str" type="text" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('no_str')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Surat Izin Praktik --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Surat Izin Praktik</span></label>
+                            <input wire:model.defer="surat_izin_pratik" type="text" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('surat_izin_pratik')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Masa Berlaku SIP --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Masa Berlaku SIP</span></label>
+                            <input wire:model.defer="masa_berlaku_sip" type="date" class="input input-bordered w-full" />
+                            <x-input-error :messages="$errors->get('masa_berlaku_sip')" class="text-error text-sm mt-1" />
+                        </div>
+
+
+                        {{-- Upload Foto --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Unggah Foto</span></label>
+                            <input type="file" id="foto_wajah" wire:model="foto_wajah" class="file-input file-input-bordered w-full" />
+                            <div class="mt-2 text-sm text-gray-500 flex items-center gap-2" wire:loading wire:target="foto_wajah">
+                                <span class="loading loading-spinner loading-sm text-info"></span>
+                                <span>Mengunggah foto...</span>
+                            </div>
+                            <x-input-error :messages="$errors->get('foto_wajah')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Upload TTD --}}
+                        <div class="form-control">
+                            <label class="label"><span class="label-text">Unggah Tanda Tangan Digital</span></label>
+                            <input type="file" id="ttd_digital" wire:model="ttd_digital" class="file-input file-input-bordered w-full" />
+                            <div class="mt-2 text-sm text-gray-500 flex items-center gap-2" wire:loading wire:target="ttd_digital">
+                                <span class="loading loading-spinner loading-sm text-info"></span>
+                                <span>Mengunggah Tanda Tangan...</span>
+                            </div>
+                            <x-input-error :messages="$errors->get('ttd_digital')" class="text-error text-sm mt-1" />
+                        </div>
+
+                        {{-- Preview Foto --}}
+                        <div class="form-control col-span-full">
+                            <label class="label"><span class="label-text">Preview Foto</span></label>
+                            @if ($foto_wajah)
+                                <img src="{{ $foto_wajah->temporaryUrl() }}" alt="Preview Foto" class="w-32 h-32 mt-2 rounded border object-cover" />
+                            @elseif ($foto_wajah_preview)
+                                <img src="{{ asset('storage/' . $foto_wajah_preview) }}" alt="Foto Wajah Lama" class="w-32 h-32 mt-2 rounded border object-cover" />
+                            @endif
+                        </div>
+
+                        {{-- Preview TTD --}}
+                        <div class="form-control col-span-full">
+                            <label class="label"><span class="label-text">Preview Tanda Tangan</span></label>
+                            @if ($ttd_digital)
+                                <img src="{{ $ttd_digital->temporaryUrl() }}" alt="Preview Foto" class="w-32 h-32 mt-2 rounded border object-cover" />
+                            @elseif ($ttd_digital_preview)
+                                <img src="{{ asset('storage/' . $ttd_digital_preview) }}" alt="Foto Wajah Lama" class="w-32 h-32 mt-2 rounded border object-cover" />
+                            @endif
+                        </div>
+                    </div>
+
+                    {{-- Tombol Simpan --}}
+                    <div class="pt-4 text-right border-t">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fas fa-save mr-2"></i> Simpan Pengguna
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
 </div>
