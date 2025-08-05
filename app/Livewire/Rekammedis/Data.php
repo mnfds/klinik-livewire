81c0bd5684bbb6d41223450fc09e7fc912b0593e
mnfds <daus2016.d2@gmail.com>
2025-08-05 15:48:21 +0800

    feat: tampilkan riwayat rekam medis beserta data kajian awal dengan tampilan accordion

--- app/Livewire/Rekammedis/Data.php
+++ app/Livewire/Rekammedis/Data.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Rekammedis;
 
 use App\Models\Pasien;
+use App\Models\PasienTerdaftar;
 use Livewire\Component;
 
 class Data extends Component
@@ -10,12 +11,17 @@ class Data extends Component
     public ?int $pasien_id = null;
     public ?Pasien $pasien = null;
 
+    public $pasienTerdaftar;
+
     public function mount($pasien_id = null)
     {
         $this->pasien_id = $pasien_id;
         if ($this->pasien_id) {
             $this->pasien = Pasien::find($this->pasien_id);
+            
+            $this->pasienTerdaftar = $this->pasien->kunjungan()->latest()->get();
         }
+
     }
 
     public function render()
