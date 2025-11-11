f2d70bfc6f466a901b9cc4aac3d87352830f860f
mnfds <daus2016.d2@gmail.com>
2025-11-11 09:30:23 +0800

    feat: Menambahkan button link WhatsApp

--- resources/views/components/action-button-layanantersisa.blade.php
+++ resources/views/components/action-button-layanantersisa.blade.php
@@ -2,4 +2,7 @@
     <a wire:navigate href="{{ route('tindaklanjut.detail', ['pasien_id' => $row->id]) }}" class="btn btn-primary">
         <i class="fa-solid fa-gifts"></i>Detail
     </a>
+    <a href="https://wa.me/{{ $row->no_telp }}" target="_blank" class="btn btn-success">
+        <i class="fa-brands fa-whatsapp"></i>WhatsApp
+    </a>
 </div>
