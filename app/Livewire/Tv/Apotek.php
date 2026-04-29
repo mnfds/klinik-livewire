b8efc92765a6f744ffbda945dddb260d79526747
mnfds <daus2016.d2@gmail.com>
2026-04-29 20:49:07 +0800

    feat: menambahkan display antrian pengambilan resep obat

--- app/Livewire/Tv/Apotek.php
+++ app/Livewire/Tv/Apotek.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Tv;
 
+use App\Models\PoliKlinik;
 use Livewire\Component;
 
 class Apotek extends Component
 {
+    public $poli;
+    
+    public function mount()
+    {
+        $this->loadAntrianApotek();
+    }
+
+    public function loadAntrianApotek()
+    {
+        $today = today()->toDateString();
+
+        $this->poli = PoliKlinik::where('status', true)
+        ->with(['pasienTerdaftars' => function ($q) use ($today){
+            $q->whereDate('created_at', $today)
+            ->where('status_terdaftar', 'peresepan')
+            ->orderBy('updated_at', 'asc')
+            ->limit(5);
+        }])->get();
+    }
+
     public function render()
     {
         return view('livewire.tv.apotek');
