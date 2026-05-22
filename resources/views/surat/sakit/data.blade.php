5cb9f128fd2ce51c46973fb6cb1b41e1cc4a1891
mnfds <daus2016.d2@gmail.com>
2026-05-22 20:09:04 +0800

    feat: Menambahkan fitur Surat Keterangan (belum selesai)

---
+++ resources/views/surat/sakit/data.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <livewire:Surat.Sakit.Data />
+    <livewire:Surat.Sakit.Store />
+    <livewire:Surat.Sakit.Update />
+</x-app-layout>
---
+++ resources/views/surat/sehat/data.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <livewire:Surat.Sehat.Data />
+    <livewire:Surat.Sehat.Store />
+    <livewire:Surat.Sehat.Update />
+</x-app-layout>
